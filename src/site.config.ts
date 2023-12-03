10cc13c7fed8456bef991bd182d8fa995bde72c8
Hewerton <hewertonlourenco@gmail.com>
2023-12-03 09:50:16 -0300

    Chore: some adjusts of the site

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,18 +2,18 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "Hewerton Lourenço",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "Hewerton",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "A simple portfolio blog.",
 	// HTML lang property, found in src/layouts/Base.astro L:18
-	lang: "en-GB",
+	lang: "pt-BR",
 	// Meta property, found in src/components/BaseHead.astro L:42
-	ogLocale: "en_GB",
+	ogLocale: "pt_BR",
 	// Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
 	date: {
-		locale: "en-GB",
+		locale: "pt-BR",
 		options: {
 			day: "numeric",
 			month: "short",
